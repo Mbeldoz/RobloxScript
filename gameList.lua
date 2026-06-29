f6a1694509f76f823e9d1a25a6009b4eb1c8e38d
Mbeldoz <emaildc17@gmail.com>
2026-06-30 06:39:57 +0700

    Update gameList.lua

--- gameList.lua
+++ gameList.lua
@@ -1,5 +1,5 @@
 return {
-    [4589469209] = "", -- GAG 2
+    [97598239454123] = "https://raw.githubusercontent.com/Mbeldoz/RobloxScript/refs/heads/main/Game/GAG2.lua", -- GAG 2
     [4924922222] = "", -- +1 Speed Keyboard
 }
 
